c66344463ef2ae727408ce021572d75a091d88a1
MatheusVII <144854722+MatheusVII@users.noreply.github.com>
2025-04-05 20:26:38 -0300

    Arquivo dos botoes de troca de tabelas
    crie em arquivo para evitar repeticoes nos outros arquivos

---
+++ btnTabelas.php
@@ -0,0 +1,3 @@
+<a href="recusados.php" class="btn btn-primary float-end" id="btn_recusados">Recusados</a>
+<a href="confirmados.php" class="btn btn-primary float-end mx-2" id="btn_confimados">Confirmados</a>
+<a href="pendentes.php" class="btn btn-primary float-end" id="btn_pendentes" onclick="">Pendentes</a>
