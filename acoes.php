4a61b1f2fe00e52e67031891f7728e5c81d5ce34
MatheusVII <144854722+MatheusVII@users.noreply.github.com>
2025-04-08 18:22:38 -0300

    Adicionei as funcoes dos outros arquivos

--- acoes.php
+++ acoes.php
@@ -11,5 +11,19 @@
         mysqli_query($conexao, $sql);
         header('Location: cadastro.php');
     }
-    
+
+    if(isset($_POST['confirmar_requisicao'])){
+        $id = mysqli_real_escape_string($conexao,($_POST['id_aluno']));
+        $sql = "UPDATE requisicoes SET status = 'confirmado' WHERE id = '$id'";
+        mysqli_query($conexao, $sql);
+        header('Location: pendentes.php');
+    }
+
+    if(isset($_POST['enviar'])){
+        $just = mysqli_real_escape_string($conexao,($_POST['justificativa']));
+        $id = mysqli_real_escape_string($conexao,($_POST['id_aluno']));
+        $sql = "UPDATE requisicoes SET status = 'recusado', justificativa = '$just' WHERE id = '$id'";
+        mysqli_query($conexao, $sql);
+        header('Location: pendentes.php');
+    }
 ?>
