a688d8087f6b3162304f216913bbb84e82aa8c0f
MatheusVII <144854722+MatheusVII@users.noreply.github.com>
2025-04-08 18:30:00 -0300

    Comentarios

--- acoes.php
+++ acoes.php
@@ -2,6 +2,7 @@
     session_start();
     require('conexao.php');
 
+    //Botao do arquivo de cadastro.php
     if(isset($_POST['cadastrar'])){
         $cpf = mysqli_real_escape_string($conexao,trim($_POST['cpf']));
         $senha = mysqli_real_escape_string($conexao,trim($_POST['senha']));
@@ -12,13 +13,15 @@
         header('Location: cadastro.php');
     }
 
+    //Acao do moderador de confirmar uma requisicao
     if(isset($_POST['confirmar_requisicao'])){
         $id = mysqli_real_escape_string($conexao,($_POST['id_aluno']));
         $sql = "UPDATE requisicoes SET status = 'confirmado' WHERE id = '$id'";
         mysqli_query($conexao, $sql);
         header('Location: pendentes.php');
     }
-
+    
+    //Botao do modal da justificativa
     if(isset($_POST['enviar'])){
         $just = mysqli_real_escape_string($conexao,($_POST['justificativa']));
         $id = mysqli_real_escape_string($conexao,($_POST['id_aluno']));
